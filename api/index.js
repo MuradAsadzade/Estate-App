83f1f7a7fc2b9fa89ed875a6c537a71abd4dbd82
MuradAsadzade <muradesedzade2004@gmail.com>
2024-07-13 15:23:54 +0400

    duplicate users solved

--- api/index.js
+++ api/index.js
@@ -6,21 +6,30 @@ import authRouter from "./routes/auth.route.js"
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.connect(process.env.MONGO).then(() => {
     console.log("Connected to MongoDB");
-}).catch((err)=>{
-    console.log("Error"+err);
+}).catch((err) => {
+    console.log("Error" + err);
 });
 
 
-
-
-const app=express();
+const app = express();
 app.use(express.json());
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("Server is running on port 3000");
 })
 
-app.use("/api/user",userRouter);
-app.use("/api/auth",authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    })
+});
