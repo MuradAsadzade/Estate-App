b42b37c76ea326962a54b60aa2295ff6a2e0d43c
MuradAsadzade <muradesedzade2004@gmail.com>
2024-07-14 01:17:32 +0400

    sign-up completed successfully

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server:{
-    port:5000
-  }
+    proxy:{
+      "/api":{
+        target:"http://localhost:3000",
+        secure:false
+      }
+    }
+  },
+  plugins: [react()]
 })
