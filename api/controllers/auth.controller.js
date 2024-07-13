d1d052b8e0b2457a9a2fe2e4fc8a66e4378d5ac7
MuradAsadzade <muradesedzade2004@gmail.com>
2024-07-13 14:52:02 +0400

    password hashed successfully

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup=async (req,res)=>{
+    const {username,email,password}=req.body;
+    const hashed_password=bcrypt.hashSync(password,10);
+    const newUser=new User({username,email,password:hashed_password});
+    
+    try {
+        await newUser.save();
+        res.status(201).json("User created successfully")
+    } catch (error) {
+        
+    }
+    
+    res.json(newUser);
+}
+
+
