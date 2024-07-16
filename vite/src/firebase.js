5eb9cf50699051db83fb86dbaec4fbda4f8ad2d9
MuradAsadzade <muradesedzade2004@gmail.com>
2024-07-16 15:41:50 +0400

    Google OAuth used

---
+++ vite/src/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey:import.meta.env.VITE_FIREBASE_API_KEY ,
+  authDomain: "mern-estate-56a1a.firebaseapp.com",
+  projectId: "mern-estate-56a1a",
+  storageBucket: "mern-estate-56a1a.appspot.com",
+  messagingSenderId: "908878954200",
+  appId: "1:908878954200:web:09e5a360c848ddfd40e1ad"
+};
+
+// Initialize Firebase
+export  const app = initializeApp(firebaseConfig);
